534a40fa6ca1a9af42f175504147980e9b653979
Juicyjames <iceandjames@gmail.com>
2020-05-12 15:43:08 -0600

    added gatsby-background-image and cleaned up started files

--- snoc_app/src/pages/index.js
+++ snoc_app/src/pages/index.js
@@ -8,13 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to The School of The Natural Order.</p>
-    <p>We are in the process of building a new website.  Please check back in a week.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    
   </Layout>
 )
 
