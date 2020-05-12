dd9bd73ec88e00cd0bc4f025872bddc080840709
Juicyjames <iceandjames@gmail.com>
2020-05-12 16:21:13 -0600

    added image of longs peak and built css to have overlay with text and subheadling

--- snoc_app/src/pages/index.js
+++ snoc_app/src/pages/index.js
@@ -1,15 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import BackgroundImage from "gatsby-background-image"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = (props) => {
+  console.log(props.data)
+  return(
   <Layout>
     <SEO title="Home" />
-    
+    <BackgroundImage 
+      className="masthead"
+      fluid={ props.data.indexImage.childImageSharp.fluid }
+    >
+      <div className="black-overlay">
+        
+        <div className="content-box">
+          <h1>Hello</h1>
+          <h2>Website under construction. Come back soon!</h2>
+        </div>
+      </div>
+    </BackgroundImage>
   </Layout>
-)
+)}
 
 export default IndexPage
+
+
+export const pageQuery = graphql`
+  query {
+    indexImage: file(relativePath: { eq: "longspeak.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1800) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
