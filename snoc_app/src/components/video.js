3bb863ec9303024f746e913ef4ae413cc32644e9
Juicyjames <iceandjames@gmail.com>
2020-05-14 22:44:02 -0600

    changed title and added first video and paragraph, also changed gatsby logo to snoc logo.

---
+++ snoc_app/src/components/video.js
@@ -0,0 +1,18 @@
+import React from "react"
+const Video = ({ videoSrcURL, videoTitle, ...props }) => (
+  <div className="video">
+    <iframe
+      src={videoSrcURL}
+      title={videoTitle}
+      width="560"
+      height="315"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      frameBorder="0"
+      webkitallowfullscreen="true"
+      mozallowfullscreen="true"
+      allowFullScreen
+    />
+  </div>
+)
+export default Video
+
--- snoc_app/src/pages/index.js
+++ snoc_app/src/pages/index.js
@@ -3,29 +3,67 @@ import BackgroundImage from "gatsby-background-image"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Video from "../components/video"
+import { Container, Row, Col } from "reactstrap"
 
-const IndexPage = (props) => {
+import "../styles/home.scss"
+
+const IndexPage = props => {
   console.log(props.data)
-  return(
-  <Layout>
-    <SEO title="Home" />
-    <BackgroundImage 
-      className="masthead"
-      fluid={ props.data.indexImage.childImageSharp.fluid }
-    >
-      <div className="black-overlay">
-        
-        <div className="content-box">
-          <h1>Hello</h1>
-          <h2>Website under construction. Come back soon!</h2>
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <BackgroundImage
+        className="masthead"
+        fluid={props.data.indexImage.childImageSharp.fluid}
+      >
+        <div className="black-overlay">
+          <div className="content-box">
+            <h1>The Teachings of Vitvan</h1>
+            <h3>The Power to be Conscious</h3>
+
+            <h3>WEBSITE UNDERCONSTRUCTION</h3>
+          </div>
         </div>
-      </div>
-    </BackgroundImage>
-  </Layout>
-)}
+      </BackgroundImage>
+      <Container fluid="md" >
+        <Row>
+          <Col md={5} style={{ textAlign: "center" }}>
+            <h3>The Power to Be Conscious</h3>
+            <p>
+              “Do you know that the most important, fundamental, basic Fact
+              about yourself is the Power to be Conscious?
+            </p>
+            <p>
+              {" "}
+              With what power do you imagine? With what power do you conceive?
+              With what power do you formulate the neural responses of stimuli
+              received or registered into images, pictures, etc.? With what
+              power do you give values, meanings (and react thereto) etc.?
+            </p>
+            <p>
+              {" "}
+              Call that Power as you wish, label it as you like…with what power
+              do you reflect, ruminate, contemplate, etc.? For the sake of
+              communication we refer to That….. by whatever name you call it
+              …..as the Power to Be Conscious’
+            </p>
+          </Col>
+          <Col md={2}></Col>
+          <Col md={5}>
+            <Video
+              videoSrcURL={"https://www.youtube.com/embed/AX4VAUIWvUE"}
+              videoTitle={"Vitvan - The Power to be Conscious"}
+            />
+          </Col>
+        </Row>
+        
+      </Container>
+    </Layout>
+  )
+}
 
 export default IndexPage
-
 
 export const pageQuery = graphql`
   query {
@@ -37,4 +75,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
